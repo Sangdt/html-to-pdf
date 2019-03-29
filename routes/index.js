1da17741b605a88cbbb30a8f04fbd0205e2d896a
Sangdt <sangsangdt25@gmail.com>
2019-03-29 22:16:14 +0700

    add some file and change the css

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,17 @@ const { JSDOM } = jsdom;
    * return 1 chuỗi html
    */
   const getHoadonHtml = ()=>{
-  var HoadonHtml = fs.readFileSync('./views/hoadon.ejs','utf-8');
-  //add thêm thông tin css để file pdf tạo ra đúng chuẩn
-  var pageInfo= '<!DOCTYPE html><html><head><title>HTml to PDF</title><link rel="stylesheet" href="https://html-to-pdf-fpt.herokuapp.com/stylesheets/style.css" /><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> </head><body>';
-  var pageEnd = "</body></html>";
-  var html= pageInfo + ejs.render(HoadonHtml)+pageEnd;
-  const htmlstring = new JSDOM(html);// tạo đối tượng html để chuẩn bị thay đổi img tag
-  var img =htmlstring.window.document.getElementById("imgLOGO");// lấy thẻ img trong html 
-  img.src="https://html-to-pdf-fpt.herokuapp.com/images/Untitled.png"
-  const finalHTML=htmlstring.window.document.documentElement.outerHTML;// trả về chuổi html đã thay đổi
+    var HoadonHtml = fs.readFileSync('./views/hoadon.ejs','utf-8');
+    //add thêm thông tin css để file pdf tạo ra đúng chuẩn
+    var pageInfo= '<!DOCTYPE html><html><head><title>HTml to PDF</title><link rel="stylesheet" href="https://html-to-pdf-fpt.herokuapp.com/stylesheets/style.css" /><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> </head><body>';
+    var pageEnd = "</body></html>";
+    var html= pageInfo + ejs.render(HoadonHtml)+pageEnd;
+    const htmlstring = new JSDOM(html);// tạo đối tượng html để chuẩn bị thay đổi img tag
+    var img =htmlstring.window.document.getElementById("imgLOGO");// lấy thẻ img trong html 
+    img.src="https://html-to-pdf-fpt.herokuapp.com/images/Untitled.png"
+    const finalHTML=htmlstring.window.document.documentElement.outerHTML;// trả về chuổi html đã thay đổi
   
-  return finalHTML;//return html string 
+    return finalHTML;//return html string 
 }
 
 // Delete file pdf cũ trc khi tạo ra 1 cái mới
@@ -36,6 +36,8 @@ const deleteFile = (filePath, callback) => {
     }
   })
 };
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'html-pdf' });
@@ -46,10 +48,12 @@ router.get('/', function(req, res, next) {
 router.get('/getPDF', function(req, res, next) {
   var htmlString= getHoadonHtml();
   var downloadFile = __dirname + '/PDF contains/pdfResult.pdf';// lấy path để gửi về client 
+
   //Xóa file pdf cũ trc khi tạo để cập nhật khi có thay đổi 
   deleteFile(downloadFile,(error)=>{
     if(error) console.log(error);
   })
+
   //tạo file pdf!
   htmlPf.create(htmlString).toFile('./routes/PDF contains/pdfResult.pdf', function(err, path) {
     if (err) return console.log(err);
